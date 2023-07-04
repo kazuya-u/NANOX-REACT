763cb8495bf5904abd643a2fa23f704a9e5da842
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-07-05 00:54:44 +0900

    Study about "program type".

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
@@ -11,11 +12,33 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const App = () => {
+interface SquareProps {
+  value: string;
+}
+
+const Square: React.FC<SquareProps> = ({value}) => {
+  return <button className='square'>{value}</button>;
+}
+
+const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <div>App</div>
+      <div className='board-row"'>
+        <Square value='1' />
+        <Square value='2' />
+        <Square value='3' />
+      </div>
+      <div className='board-row"'>
+        <Square value='4' />
+        <Square value='5' />
+        <Square value='6' />
+      </div>
+      <div className='board-row"'>
+        <Square value='7' />
+        <Square value='8' />
+        <Square value='9' />
+      </div>
     </>
   );
 };
