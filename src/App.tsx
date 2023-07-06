66e0b81be1963db855b02132dbd689edc239aa5b
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-07-06 23:28:41 +0900

    Changed to manage state in parent component.

--- src/App.tsx
+++ src/App.tsx
@@ -12,43 +12,34 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-interface SquareProps {
-  value?: string;
-}
-
-const Square: React.FC<SquareProps> = ({ value = '' }) => {
-  const [squareValue, setSquarevalue] = useState<string | null>(null);
-  function handleClick() {
-    setSquarevalue('x');
-  }
-  return (
-    <button
-     className='square'
-     onClick={handleClick}
-    >
-      {squareValue || value}
-    </button>
-  )
+function Square({value, onSquareClick}) {
+  return <button className='square' onClick={onSquareClick}>{value}</button>
 }
 
 const App: React.FC = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'x';
+    setSquares(nextSquares);
+  }
   return (
     <>
       <GlobalStyle />
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
