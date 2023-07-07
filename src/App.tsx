9d326f50630281a66d212eecbd8ef3a1253b2eee
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-07-08 01:57:11 +0900

    Completion of the game.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
-import React, { useState } from 'react';
-import reset from 'styled-reset';
+import { createGlobalStyle } from "styled-components";
+import React, { useState } from "react";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -18,25 +18,59 @@ interface SquareProps {
 }
 
 const Square: React.FC<SquareProps> = ({ value, onSquareClick }) => {
-  return <button className='square' onClick={onSquareClick}>{value}</button>
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 const App: React.FC = () => {
-  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
+  const [squares, setSquares] = useState<Array<string | null>>(
+    Array(9).fill(null)
+  );
   const [xIsNext, setXIsNext] = useState(true);
   const handleClick = (i: number): void => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
-    nextSquares[i] = xIsNext ? 'x' : 'o';
+    nextSquares[i] = xIsNext ? "x" : "o";
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
+  };
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
   }
   return (
     <>
       <GlobalStyle />
-      <div className='board'>
+      <div className="status">{status}</div>
+      <div className="board">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
