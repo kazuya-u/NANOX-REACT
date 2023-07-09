afd49bd17f8fdd6530377e71d3dac3b52eab0a83
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-07-10 01:14:21 +0900

    Implements Error Page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './style.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/root'
+import ErrorPageWrapper from './error-page'
+import Contact from "./routes/contact";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPageWrapper />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
