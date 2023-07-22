1c2131262bf71604b8cacf8b5773a9d81e9a7f59
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-07-22 17:24:23 +0900

    Implements conditional routing.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,28 +1,68 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+type NavHiddenProps = {
+  hidden?: boolean
+}
+
+const SideMenuWrapper = styled.ul<NavHiddenProps>`
+display: ${({ hidden }) => hidden ? 'none' : 'block'} ;
+`;
 
 const Root: React.FC = () => {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link to={"/"}>Home</Link>
-        </li>
-        <li>
-          <Link to={"/posts"}>Posts</Link>
-        </li>
-        <li>
-          <Link to={"/about"}>About</Link>
-        </li>
-        <li>
-          <Link to={"/contact"}>Contact</Link>
-        </li>
-        <li>
-          <Link to={"/login"}>Login</Link>
-        </li>
-      </ul>
-      <Outlet />
-    </>
-  );
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  if (currentPath === '/login') {
+    return (
+      <>
+        <SideMenuWrapper hidden>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/posts"}>Posts</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+        </SideMenuWrapper>
+        <Outlet />
+      </>
+    );
+  }
+  else {
+    return (
+      <>
+        <SideMenuWrapper>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/posts"}>Posts</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+        </SideMenuWrapper>
+        <Outlet />
+      </>
+    );
+  }
+  
+
 };
 
 export default Root
