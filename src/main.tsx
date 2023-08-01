b693d893b2d70eeb12a4b98a6af7fb0d862d2cb7
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-08-02 02:56:28 +0900

    Adjustment directory structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import './style.css'
+import { App } from './App';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from "react-router-dom";
-import React from "react";
-import RooterConfig from "./Route/RooterConfig";
 
 const root: HTMLElement | null = document.getElementById('root');
 
 if (root) {
   createRoot(root).render(
-    <React.StrictMode>
-      <RouterProvider router={RooterConfig} />
-    </React.StrictMode>,
+    <App />
   )
 }
