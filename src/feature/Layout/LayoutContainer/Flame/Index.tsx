43e28634d30261d58a5c1fc9c2cf8e39e66d5172
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-10-01 00:11:29 +0900

    Styled flame area.

---
+++ src/feature/Layout/LayoutContainer/Flame/Index.tsx
@@ -0,0 +1,72 @@
+import { Outlet } from "react-router-dom"
+import styled from "styled-components"
+
+export default function Flame() {
+  return (
+    <StyledFlame>
+      <StyledFlameContainer>
+        <StyledFlameWrapper>
+          <StyledFlameInner>
+            <StyledFlameInnerWrapper>
+              <Outlet />
+            </StyledFlameInnerWrapper>
+          </StyledFlameInner>
+        </StyledFlameWrapper>
+      </StyledFlameContainer>
+    </StyledFlame>
+  )
+}
+
+const StyledFlame = styled.div`
+  flex-grow: 0;
+  flex-shrink: 1;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  z-index: 1;
+  height: calc(100vh - 45px);
+  max-height: 100%;
+  position: relative;
+  width: 1208px;
+  transition-property: width;
+  transition-duration: 270ms;
+  transition-timing-function: ease;
+`;
+
+
+const StyledFlameContainer = styled.div`
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  position: relative;
+  align-items: center;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  overflow: hidden auto;
+`;
+
+const StyledFlameWrapper = styled.div`
+  margin-top: 120px;
+  caret-color: rgb(55, 53, 47);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  align-items: center;
+  flex-grow: 1;
+`;
+
+const StyledFlameInner = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 3;
+  flex-shrink: 0;
+`;
+
+const StyledFlameInnerWrapper = styled.div`
+  max-width: 100%;
+  min-width: 0px;
+  width: 900px;
+`;
