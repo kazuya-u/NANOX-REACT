5d8dbbce5aed7c93de932492723ddc382bd2b018
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-09-30 18:44:55 +0900

    Coding layout.

---
+++ src/feature/Layout/LayoutContainer/Index.tsx
@@ -0,0 +1,51 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../../feature/Navbar/NavBar";
+import styled from "styled-components";
+
+const LayoutContainer: React.FC = () => {
+  return (
+    <>
+      <StyledContainer>
+        <StyledInner>
+          <StyledInnerWrapper>
+            <StyledListner>
+              <Navbar />
+              <Outlet />
+            </StyledListner>
+          </StyledInnerWrapper>
+        </StyledInner>
+      </StyledContainer>
+    </>
+  );
+};
+
+const StyledContainer = styled.div`
+  overflow: hidden;
+  position: relative;
+  height: 100vh;
+`;
+
+const StyledInner = styled.div`
+  color: #37352f;
+  fill: currentcolor;
+  line-height: 1.5;
+  font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Hiragino Sans", "Hiragino Kaku Gothic ProN", "Hiragino Sans GB", メイリオ, Meiryo, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
+  -webkit-font-smoothing: auto;
+  background-color: #fff;
+`;
+
+const StyledInnerWrapper = styled.div`
+  height: 100%;
+`;
+
+const StyledListner = styled.div`
+  width: 100vw;
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex: 1 1 0%;
+  background: white;
+  cursor: text;
+`;
+
+export default LayoutContainer;
