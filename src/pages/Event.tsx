280c1b239170fd4e64d5a7ea0e29fc42dfb6313f
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-10-23 02:35:41 +0900

    Fixed error handling. Add styled component.

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -2,11 +2,11 @@
 import googleCalendarPlugin from '@fullcalendar/google-calendar'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import FullCalendar from '@fullcalendar/react';
-import styled from 'styled-components';
 import { getUserSettingsIdFromLocalStorage } from '../feature/AuthUser/utils/LocalStorageUtils';
 import { useFetchDataNoMutate } from '../utils/fetchData';
 import { BASE_API_URL } from '../utils/EndPoint';
 import { Oval } from 'react-loader-spinner'
+import { StyleCalendar } from '../components/Calendar/styled';
 
 interface UsData {
   data: {
@@ -40,7 +40,7 @@ const Event: React.FC = () => {
   if (!SettinsData) {
     return (
       <>
-        <StyledLoading>
+        <div>
           <Oval
             height={80}
             width={80}
@@ -53,23 +53,30 @@ const Event: React.FC = () => {
             strokeWidth={2}
             strokeWidthSecondary={2}
           />
-        </StyledLoading>
+        </div>
       </>
     )
   }
-  const api_key = SettinsData?.data.attributes.field_google_calender[0].key;
-  const googleCalendarId = {
-    googleCalendarApiKey: api_key,
-    googleCalendarId: SettinsData?.data.attributes.field_google_calender[0].value,
-  };
+  if (!SettinsData?.data.attributes.field_google_calender) {
+    return (
+      <>
+        カレンダーの設定が不足しています。
+      </>
+    )
+  }
+  const eventData = SettinsData?.data.attributes.field_google_calender.map(i => {
+    return {
+      googleCalendarApiKey: i.key,
+      googleCalendarId: i.value,
+    };
+  });
+  
   return (
     <StyleCalendar>
       <FullCalendar
         plugins={[dayGridPlugin, googleCalendarPlugin]}
         initialView="dayGridMonth"
-        eventSources={
-          [googleCalendarId]
-        }
+        eventSources={eventData}
         eventDisplay='block'
         editable
         timeZone='Asia/Tokyo'
@@ -86,53 +93,5 @@ const Event: React.FC = () => {
     </StyleCalendar>
   );
 }
-
-const StyledLoading = styled.div`
-  position: fixed;
-`
-
-const StyleCalendar = styled.div`
-.fc.fc-media-screen.fc-direction-ltr.fc-theme-standard {
-  position: relative;
-  padding-left: 1px;
-}
-.fc-scrollgrid-section.fc-scrollgrid-section-header  {
-  margin-top: 0px;
-}
-.fc-col-header-cell.fc-day {
-  font-size: 12px;
-  color: #37352f7f;
-  font-weight: 400;
-}
-.fc-daygrid-day-frame.fc-scrollgrid-sync-inner {
-  position: relative; flex: 1 0 0px; cursor: default;
-}
-.fc-daygrid-day-number {
-  font-size: 14px; height: 24px; line-height: 24px; text-align: right; transition: color 100ms ease-out 0s;
-}
-
-.fc-daygrid-day-events {
-  margin-top: 2px;
-  margin-left: 2px;
-  margin-right: 2px;
-}
-.fc-event.fc-event-draggable.fc-event-start.fc-event-end.fc-event-today.fc-daygrid-event.fc-daygrid-block-event.fc-h-event {
-  padding: 3px 6px;
-  font-weight: 400;
-  display: flex;
-  text-decoration: none;
-  user-select: none;
-  cursor: pointer;
-  height: 100%;
-  border-radius: 3px;
-  box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 2px 4px;
-  align-items: flex-start;
-  flex-direction: column;
-}
-
-.fc-event-time {
-  font-weight: 400;
-}
-`;
 
 export default Event;
