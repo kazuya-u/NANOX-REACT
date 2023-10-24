2ccf9a41298ced2bb37e300aa855804154f5a634
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-10-25 01:40:26 +0900

    Fixed error message.

--- src/utils/api/UserProvider.tsx
+++ src/utils/api/UserProvider.tsx
@@ -45,7 +45,7 @@ export const UserContext = createContext<UserContextType | undefined>(undefined)
 export function useUser() {
   const context = useContext(UserContext);
   if (context === undefined) {
-    throw new Error('useUser hook is being used outside of the UserProvider context. Make sure to use this hook within a component that is wrapped with UserProvider.');
+    throw new Error('throw new Error(`useUser hook is being used outside of the UserProvider context in ${Component.name}. Make sure to use this hook within a component that is wrapped with UserProvider.`);');
   }
   return context.user;
 }
