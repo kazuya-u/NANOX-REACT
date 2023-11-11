a9609c2dd384aba6c9c28e318b2f425445d96ea3
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-11-12 04:29:17 +0900

    Implemented task list.

--- src/feature/Task/List/Index.tsx
+++ src/feature/Task/List/Index.tsx
@@ -1,12 +1,35 @@
+import styled from "styled-components";
 import List from "./List";
 
 const Index: React.FC = () => {
   return (
     <>
-      {/* <TaskFilter /> */}
-      <List />
+      <StyledList>
+        <StyledListWrapper>
+          <StyledListInner>
+            <StyledListInnerWrapper>
+              <List />
+            </StyledListInnerWrapper>
+          </StyledListInner>
+        </StyledListWrapper>
+        
+      </StyledList>
     </>
   );
 };
+
+const StyledList = styled.div`
+  flex-grow: 1; flex-shrink: 0; display: flex; flex-direction: column; position: relative;
+`;
+const StyledListWrapper = styled.div`
+  z-index: 1; flex-grow: 1; flex-shrink: 0; margin-right: 0px; margin-bottom: 0px;
+`;
+const StyledListInner = styled.div`
+  position: relative; float: left; min-width: 100%; user-select: none; font-variant-numeric: lining-nums tabular-nums; padding-bottom: 180px; padding-left: 96px; padding-right: 96px;
+`;
+const StyledListInnerWrapper = styled.div`
+  position: relative;
+`;
+
 
 export default Index;
--- src/feature/Task/List/List.tsx
+++ src/feature/Task/List/List.tsx
@@ -1,77 +1,216 @@
-import { Deadline, ListItem, ListWrapper, ProjectName, Status, StyledLink, Tag, TagContainer, TaskDescription, TaskLeftWrapper, TaskName, TaskRightWrapper } from "./StyledComponents";
+import { Link } from "react-router-dom";
 import { BASE_API_URL } from "../../../utils/EndPoint";
 import { useFetchData } from "../../../utils/fetchData";
-import { useState } from "react";
-import Select from 'react-select';
-import { mutate } from "swr";
+import styled from "styled-components";
+import { RenderCategory, RenderMultipleCategory } from "../../../feature/View/Utils/Utility";
 
-type ItemType = {
-  field_deadline: string;
-  field_description: string;
-  field_ref_status: string;
-  nid: string;
-  ProjectName: string;
-  TagName: string;
+interface FieldRef {
+  color_label: string;
   title: string;
-  uuid: string;
 }
 
-type OptionType = {
-  label: string,
-  value: string,
+interface NodeData {
+  Nid: string;
+  UUID: string;
+  Title: string;
+  Tag: FieldRef[];
+  Project: FieldRef;
+  Status: FieldRef;
+  field_deadline: string;
+  field_description: string;
 }
 
 const List: React.FC = () => {
-  const [filterParam, setFilterParam] = useState<OptionType>({ label: 'All', value: '' });
-  const { data, error } = useFetchData<ItemType[]>(`${BASE_API_URL}/tasks?field_ref_project_target_id=${filterParam.value}`);
-  const { data: ProjectFetchData } = useFetchData<OptionType[]>(`${BASE_API_URL}/project`);
-  const handleOptionChange = (data: OptionType | null) => {
-    if (data) {
-      setFilterParam(data);
-      mutate(`${BASE_API_URL}/tasks`);
-    }
-  }
-  if (!data && !error) {
+  // const [filterParam, setFilterParam] = useState<OptionType>({ label: 'All', value: '' });
+  const { data: TaskData, error } = useFetchData<NodeData[]>(`${BASE_API_URL}/tasks`);
+  // const { data: ProjectFetchData } = useFetchData<OptionType[]>(`${BASE_API_URL}/project`);
+  // const handleOptionChange = (data: OptionType | null) => {
+  //   if (data) {
+  //     setFilterParam(data);
+  //     mutate(`${BASE_API_URL}/tasks`);
+  //   }
+  // }
+  if (!TaskData && !error) {
     return <div>Loading...</div>
   }
-  if (!data) {
+  if (!TaskData) {
     return <div>Loading...</div>;
   }
-  if (!ProjectFetchData) {
-    return <div>Loading...</div>;
-  }
-  const newItem = { label: 'All', value: '' };
-  const updatedData = [...ProjectFetchData, newItem];
+  // if (!ProjectFetchData) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // const newItem = { label: 'All', value: '' };
+  // const updatedData = [...ProjectFetchData, newItem];
+  
 
   return (
     <>
-      <div>
+      <StyledListContainer>
+        <StyledListWrapper>
+          {TaskData.map((item: NodeData) => (
+            <StyledListItem key={item.Nid}>
+              <StyledListItemRow to={`/tasks/${item.UUID}`}>
+                {/* About Title */}
+                <StyledCell1>
+                  <StyledCell1Wrapper>
+                    <StyledTitle>
+                      <StyledTitleWrapper>
+                        {item.Title}
+                      </StyledTitleWrapper>
+                    </StyledTitle>
+                  </StyledCell1Wrapper>
+                </StyledCell1>
+
+                {/* About Project */}
+                <StyledCell2>
+                  <StyledCell2Wrapper>
+                    <StyledCategory>
+                      <StyledCategoryWrapper>
+                        <RenderCategory data={item.Project} />
+                      </StyledCategoryWrapper>
+                    </StyledCategory>
+                  </StyledCell2Wrapper>
+                </StyledCell2>
+
+                {/* About Tags */}
+                <StyledCell2>
+                  <StyledCell2Wrapper>
+                    <StyledCategory>
+                      <StyledCategoryWrapper>
+                        <RenderCategory data={item.Tag} isMultiple={true} />
+                      </StyledCategoryWrapper>
+                    </StyledCategory>
+                  </StyledCell2Wrapper>
+                </StyledCell2>
+
+                {/* About Status */}
+                <StyledCell2>
+                  <StyledCell2Wrapper>
+                    <StyledCategory>
+                      <StyledCategoryWrapper>
+                        <RenderCategory data={item.Status} />
+                      </StyledCategoryWrapper>
+                    </StyledCategory>
+                  </StyledCell2Wrapper>
+                </StyledCell2>
+
+              </StyledListItemRow>
+              {/* <StyledLink to={`/tasks/${item.uuid}`}>
+                    <TaskLeftWrapper>
+                      <TaskName>{item.title}</TaskName>
+                      <TaskDescription>{item.field_description}</TaskDescription>
+                      {item.TagName ? <TagContainer><Tag>{item.TagName}</Tag></TagContainer> : ''}
+                    </TaskLeftWrapper>
+                    <TaskRightWrapper>
+                      <Deadline>Due: {item.field_deadline}</Deadline>
+                      <Status>Status: {item.field_ref_status}</Status>
+                      <ProjectName>{item.ProjectName}</ProjectName>
+                    </TaskRightWrapper>
+                  </StyledLink> */}
+            </StyledListItem>
+          ))}
+        </StyledListWrapper>
+      </StyledListContainer>
+      {/* <div>
         <Select
           defaultValue={filterParam}
           onChange={handleOptionChange}
           options={updatedData}
         />
-      </div>
-      <ListWrapper>
-        {data.map((item: ItemType) => (
-          <ListItem key={item.nid}>
-            <StyledLink to={`/tasks/${item.uuid}`}>
-              <TaskLeftWrapper>
-                <TaskName>{item.title}</TaskName>
-                <TaskDescription>{item.field_description}</TaskDescription>
-                {item.TagName ? <TagContainer><Tag>{item.TagName}</Tag></TagContainer> : ''}
-              </TaskLeftWrapper>
-              <TaskRightWrapper>
-                <Deadline>Due: {item.field_deadline}</Deadline>
-                <Status>Status: {item.field_ref_status}</Status>
-                <ProjectName>{item.ProjectName}</ProjectName>
-              </TaskRightWrapper>
-            </StyledLink>
-          </ListItem>
-        ))}
-      </ListWrapper>
+      </div> */}
+
     </>
   );
 };
+
+const StyledListContainer = styled.div`
+  position: relative;
+`;
+
+const StyledListWrapper = styled.div`
+  position: relative;
+  min-width: calc((100% - 192px) - 0px);
+`;
+
+const StyledListItem = styled.div`
+  display: flex;
+  height: calc(100% + 2px);
+  border-bottom: 1px solid rgb(233, 233, 231);
+`;
+const StyledListItemRow = styled(Link)`
+  display: flex;
+`;
+const StyledCell1 = styled.div`
+  display: flex;
+  width: 276px;
+  height: calc(100% + 1px);
+  position: relative;
+  border-right: 1px solid rgb(233, 233, 231);
+`;
+
+const StyledCell1Wrapper = styled.div`
+  display: flex; overflow-x: clip; height: 100%; width: 276px;
+`;
+
+const StyledCell2 = styled.div`
+  display: flex;
+  width: 112px;
+  height: calc(100% + 1px);
+  position: relative;
+  border-right: 1px solid rgb(233, 233, 231);
+`;
+
+const StyledCell2Wrapper = styled.div`
+  display: flex;
+  overflow-x: clip;
+  height: 100%;
+  width: 112px;
+`;
+
+const StyledTitle = styled.div`
+  user-select: none;
+  transition: background 20ms ease-in 0s;
+  position: relative;
+  display: block;
+  font-size: 14px;
+  overflow: clip;
+  width: 100%;
+  white-space: normal;
+  min-height: 32px;
+  padding: 5px 8px 5px 0px;
+`;
+
+const StyledTitleWrapper = styled.div`
+  line-height: 1.5;
+  white-space: pre-wrap;
+  word-break: break-word;
+  display: inline;
+  font-weight: 500;
+  background-image: linear-gradient(to right, rgba(55, 53, 47, 0.16) 0%, rgba(55, 53, 47, 0.16) 100%);
+  background-repeat: repeat-x;
+  background-position: 0px 100%;
+  background-size: 100% 1px;
+`;
+
+const StyledCategory = styled.div`
+  user-select: none;
+  transition: background 20ms ease-in 0s;
+  cursor: pointer;
+  position: relative;
+  display: block;
+  font-size: 14px;
+  overflow: clip;
+  width: 100%;
+  white-space: normal;
+  min-height: 32px;
+  padding: 6px 8px;
+`;
+const StyledCategoryWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 6px 8px;
+`;
+
 
 export default List;
